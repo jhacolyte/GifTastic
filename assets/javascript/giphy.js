c7b763b3c441427b3a6c883fcf2204db0dbc8a6d
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-07-23 01:05:36 -0700

    still working on javascript

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -26,3 +26,34 @@ $("#addShow").on("click", function() {
 
     return false;
 })
+
+function displayGifs() {
+    var show = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&limit=9&api_key=dc6zaTOxFJmzC";
+
+
+    $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
+        console.log(response.data);
+
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+
+            var gifDiv = $('<div class=gifs>');
+            var showGif = $('<img>');
+            showGif.attr('src', results[i].images.fixed_height_still.url);
+
+            showGif.attr('title', "Rating: " + results[i].rating);
+            showGif.attr('data-still', results[i].images.fixed_height_still.url);
+            showGif.attr('data-state', 'still');
+            showGif.addClass('gif');
+            showGif.attr('data-animate', results[i].images.fixed_height.url);
+            // var rating = results[i].rating;
+            // var p = $('<p>').text('Rating: ' + rating);
+            gifDiv.append(showGif)
+                // gifDiv.append(p)
+
+            $("#gifsView").prepend(gifDiv);
+        }
+
+    });
